4d1264ec0380b83b06236f19d0799f4ca413a467
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-03-17 16:31:42 -0400

    'aside' element created with form and css transtion

--- README.md
+++
@@ -1,1 +0,0 @@
-#Cars Inventory App
---
+++ index.js
@@ -0,0 +1,2 @@
+const openNav = () => document.getElementById("form").style.width = "100%";
+const closeNav = () => document.getElementById("form").style.width = "0%";
